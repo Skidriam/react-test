aaeba953f6903c54b83a2f8643e4ad4310945f6d
Skidriam <Skidriam@gmail.com>
2021-05-24 09:35:41 +0300

    Table with hooks

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ export const fetchApi = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(response => {
+        console.log(response);
         if(response.ok) {
           return response.json();
         } else {
@@ -23,8 +24,8 @@ export default class CallApi {
     return fetchApi("https://api.npoint.io/324f4ca2cdd639760638");
   }
 
-  static getCurrencyData(){
-    return fetchApi("http://api.exchangeratesapi.io/v1/latest?access_key=fe414feb63fe888fecfc75f6234266cb&format=1");
-  }
+  // static getCurrencyData(){
+  //   return fetchApi("http://api.exchangeratesapi.io/v1/latest?access_key=fe414feb63fe888fecfc75f6234266cb&format=1");
+  // }
 
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,169 +1,151 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CallApi from "../api";
 import Checkbox from "./Checkbox";
 import Row from "./Row";
 import Button from "./Button";
 
-export default class Table extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        people: [],
-        checked: []
+export default function Table(props) {
+  const [state, setState] = useState({
+    people: [],
+    checked: []
+  });
+
+  const colsList = ["№", "ФИО", "Возраст(лет)", "Рост", "Вес", "Зарплата", ""]
+
+  const convertHeight = (oldHeight) => {//TODO проверить правильность расчета вручную
+    let pd = oldHeight.replace("\"", "");
+    pd = pd.split("'");
+    let smFull = pd[0]*30.48 + pd[1]*2.54;
+    let mSm = 0;
+    if(smFull > 200)
+      mSm = "2м " + Math.round(smFull-200) + "см";
+    else if(smFull >100)
+      mSm = "1м " + Math.round(smFull-100) + "см";
+    else
+      mSm = smFull + "см";
+    return mSm;
+  }
+
+  const convertWeight = (oldWeight) => {
+    const weightKg = Math.round(oldWeight * 0.453592);
+    return weightKg + " кг";
+  }
+
+  // const convertSalary = (eur) => {
+  //     const salaryUSD = Math.round(eur * state.currencyData.rates.USD);
+  //     return "$" + salaryUSD;
+  // }
+
+  const convertDate = (birth_date) => {
+    return Math.round((Date.now()/1000 - birth_date) / (3600 * 24 * 365))
+  }
+
+  const handleRowCheck = (id, event) => {
+    const checked = event.target.checked;
+    let newCheckedState = state.checked.slice();
+    if(event.nativeEvent.shiftKey){
+      if(state.checked.includes(true)){
+        const lastChecked = state.checked.indexOf(true);
+          newCheckedState = newCheckedState.map((elem, i) => {
+            if((i >= id && i <= lastChecked) || elem === true) {
+              return elem = true;
+            }
+            else if((i <= id && i >= lastChecked)) {
+              return elem = true;
+            }
+            return false;
+          });
       }
     }
-  
-    getPeopleDetails(){
-      CallApi.getPeopleData()
-        .then(data => {
-          this.preparePeopleDetails(data);
-        });
-    }
-  
-    convertHeight(oldHeight) {//TODO проверить правильность расчета вручную
-      let pd = oldHeight.replace("\"", "");
-      pd = pd.split("'");
-      let smFull = pd[0]*30.48 + pd[1]*2.54;
-      let mSm = 0;
-      if(smFull > 200)
-        mSm = "2м " + Math.round(smFull-200) + "см";
-      else if(smFull >100)
-        mSm = "1м " + Math.round(smFull-100) + "см";
-      else
-        mSm = smFull + "см";
-      return mSm;
-    }
-  
-    convertWeight(oldWeight) {
-      const weightKg = Math.round(oldWeight * 0.453592);
-      return weightKg + " кг";
-    }
+    newCheckedState[id] = checked;
+    setState({...state, checked: newCheckedState})
+  }
 
-    convertSalary(eur) {
-       const salaryUSD = Math.round(eur * this.state.currencyData.rates.USD);
-       return "$" + salaryUSD;
-    }
+  const handleHeadingCheck = (event) => {
+    const checked = event.target.checked;
+    let newChecked = state.checked.slice();
+    newChecked = newChecked.fill(checked);
+    setState({...state, checked: newChecked});
+  }
 
-    convertDate(birth_date) {
-      return Math.round((Date.now()/1000 - birth_date) / (3600 * 24 * 365))
-    }
-  
-    preparePeopleDetails(){
-      const data = this.state.people.slice();
+  const handleRowRemoveClick = (id) => {
+    let newChecked = state.checked.filter((item, index) => index !== id);
+    let newPeople = state.people.filter((item, index) => index !== id);
+    setState({people: newPeople, checked: newChecked});
+  }
+
+  const handleRemoveChecked = () => {
+    let checkedIds = state.checked.slice().reduce((arr, elem, index) => {
+      if(elem === true) arr.push(index);
+        return arr;
+    }, []);
+    let newChecked = state.checked.filter((item, index) => !checkedIds.includes(index));
+    let newPeople = state.people.filter((item, index) => !checkedIds.includes(index));
+    setState({people: newPeople, checked: newChecked})
+  }
+
+  useEffect(() => {
+    const preparePeopleDetails = (peopleData) => {
+      const data = peopleData;
       let loadedPeople = [];
       data.forEach((element, i) => {
         let person = {};
         person.full_name = element.first_name + " " + element.last_name;
-        person.age = this.convertDate(element.date_of_birth);
-        person.height = this.convertHeight(element.height);
-        person.weight = this.convertWeight(element.weight);
-        person.salary = this.convertSalary(element.salary);//TODO перевести в $
+        person.age = convertDate(element.date_of_birth);
+        person.height = convertHeight(element.height);
+        person.weight = convertWeight(element.weight);
+        person.salary = "EUR: " + element.salary;
         loadedPeople.push(person);
       });
-      this.setState({
+      setState({
         people: loadedPeople, 
         checked: new Array(loadedPeople.length).fill(false)
       });
     }
-  
-    handleRowCheck(id, event){
-      const checked = event.target.checked;
-      let newCheckedState = this.state.checked.slice();
-      if(event.nativeEvent.shiftKey){
-        if(this.state.checked.includes(true)){
-          const lastChecked = this.state.checked.indexOf(true);
-            newCheckedState = newCheckedState.map((elem, i) => {
-              if((i >= id && i <= lastChecked) || elem === true) {
-                return elem = true;
-              }
-              else if((i <= id && i >= lastChecked)) {
-                return elem = true;
-              }
-              return false;
-            });
-        }
-      }
-      newCheckedState[id] = checked;
-      this.setState({checked: newCheckedState});
-    }
-  
-    handleHeadingCheck(event){
-      const checked = event.target.checked;
-      let newChecked = this.state.checked.slice();
-      newChecked = newChecked.fill(checked);
-      this.setState({checked: newChecked});
-    }
-  
-    handleRowRemoveClick(id){
-      let newChecked = this.state.checked.filter((item, index) => index !== id);
-      let newPeople = this.state.people.filter((item, index) => index !== id);
-      this.setState({people: newPeople, checked: newChecked});
-    }
-  
-    handleRemoveChecked(){
-      let checkedIds = this.state.checked.slice().reduce((arr, elem, index) => {
-        if(elem === true) arr.push(index);
-          return arr;
-      }, []);
-      let newChecked = this.state.checked.filter((item, index) => !checkedIds.includes(index));
-      let newPeople = this.state.people.filter((item, index) => !checkedIds.includes(index));
-      this.setState({people: newPeople, checked: newChecked});
-    }
-  
-    componentDidMount() {
-      Promise.all([
-        CallApi.getPeopleData(),
-        CallApi.getCurrencyData(),
-      ]).then(([peopleRes, currencyRes]) => {
-        this.setState({
-          people: peopleRes,
-          currencyData: currencyRes
-        }, () => {
-          this.preparePeopleDetails();
-        });
-      });
-    }
-  
-    render() {//выкинуть все это в компонент table, слишком грязно, тут оставить только App, в котором будет исключительно <Table />
-      const colsList = ["№", "ФИО", "Возраст(лет)", "Рост", "Вес", "Зарплата", ""]
-  
-      return(
-        <React.Fragment>
-          <h1>
-            Таблица пользователей
-          </h1>
-            <table>
-              <thead>
-                <tr>
-                  <th>
-                  <Checkbox 
-                    onCheckChange={(event) => this.handleHeadingCheck(event)}
-                    checked={!this.state.checked.includes(false)}
-                  />
-                  </th>
-                  {colsList.map((item, index) => (
-                    <th key={index}>{item}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-              {this.state.people.map((element, index) => (
-                <Row 
-                  key={index}
-                  id={index+1}
-                  person={element}
-                  onCheckStateChanged={(event) => this.handleRowCheck(index, event)}
-                  onRemove={() => this.handleRowRemoveClick(index)}
-                  checked={this.state.checked[index]}
-                />
+
+    Promise.all([
+      CallApi.getPeopleData(),
+    ]).then(([peopleRes]) => {
+      preparePeopleDetails(peopleRes);
+    });
+  }, []);
+
+  return(
+    <React.Fragment>
+      <h1>
+        Таблица пользователей
+      </h1>
+        <table>
+          <thead>
+            <tr>
+              <th>
+              <Checkbox 
+                onCheckChange={(event) => handleHeadingCheck(event)}
+                checked={!state.checked.includes(false)}
+              />
+              </th>
+              {colsList.map((item, index) => (
+                <th key={index}>{item}</th>
               ))}
-            </tbody>
-            </table>
-          <Button
-            onDeleteClick={() => this.handleRemoveChecked()}
-            disabled={!this.state.checked.includes(true)}
-          />
-        </React.Fragment>
-      );
-    }
-  }
+            </tr>
+          </thead>
+          <tbody>
+          {state.people.map((element, index) => (
+            <Row 
+              key={index}
+              id={index+1}
+              person={element}
+              onCheckStateChanged={(event) => handleRowCheck(index, event)}
+              onRemove={() => handleRowRemoveClick(index)}
+              checked={state.checked[index]}
+            />
+          ))}
+        </tbody>
+        </table>
+      <Button
+        onDeleteClick={() => handleRemoveChecked()}
+        disabled={!state.checked.includes(true)}
+      />
+    </React.Fragment>
+  );
+}
